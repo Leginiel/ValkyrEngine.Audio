6daf655bc7ab6744ffd6a8e2cfcb4e203f0f66f6
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-05-13 21:30:24 +0200

    nullaudio subsystem tests

---
+++ ValkyrEngine.Audio.Tests/Resources/NullAudio/NullAudioResourceFactoryTest.cs
@@ -0,0 +1,51 @@
+﻿using ValkyrEngine.Audio.Resources;
+using ValkyrEngine.Audio.Resources.NullAudio;
+using Xunit;
+
+namespace ValkyrEngine.Audio.Tests.Resources.NullAudio
+{
+  public class NullAudioResourceFactoryTest
+  {
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestCreateAudioBuffer_ReturnNullAudioBuffer()
+    {
+      // Arrange
+      NullAudioResourceFactory factory = new NullAudioResourceFactory();
+
+      // Act
+      IAudioBuffer buffer = factory.CreateAudioBuffer();
+
+      // Assert
+      Assert.IsType<NullAudioBuffer>(buffer);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestCreateAudioSource_ReturnNullAudioSource()
+    {
+      // Arrange
+      NullAudioResourceFactory factory = new NullAudioResourceFactory();
+
+      // Act
+      IAudioSource source = factory.CreateAudioSource();
+
+      // Assert
+      Assert.IsType<NullAudioSource>(source);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestCreateAudioEngine_ReturnNullAudioEngine()
+    {
+      // Arrange
+      NullAudioResourceFactory factory = new NullAudioResourceFactory();
+
+      // Act
+      IAudioEngine engine = factory.CreateAudioEngine();
+
+      // Assert
+      Assert.IsType<NullAudioEngine>(engine);
+    }
+  }
+}
---
+++ ValkyrEngine.Audio.Tests/Resources/NullAudio/NullAudioSubSystemTest.cs
@@ -0,0 +1,32 @@
+﻿using ValkyrEngine.Audio.Resources.NullAudio;
+using Xunit;
+
+namespace ValkyrEngine.Audio.Tests.Resources.NullAudio
+{
+  public class NullAudioSubSystemTest
+  {
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestName_NoInput_ReturnsTheRightName()
+    {
+      // Arrange
+      NullAudioSubSystem subSystem = new NullAudioSubSystem();
+
+      // Act
+      // Assert
+      Assert.Equal("No audio", subSystem.Name);
+    }
+
+    [Fact]
+    [Trait("Category", "Unit")]
+    public void TestName_NoInput_ReturnsTheRightResourceFactory()
+    {
+      // Arrange
+      NullAudioSubSystem subSystem = new NullAudioSubSystem();
+
+      // Act
+      // Assert
+      Assert.IsType<NullAudioResourceFactory>(subSystem.ResourceFactory);
+    }
+  }
+}
